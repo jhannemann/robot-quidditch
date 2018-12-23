fad92e65f9337843e13e596736c0dfd340993a30
Jens Hannemann <j.hannemann@ieee.org>
2018-12-23 18:23:34 -0500

    Add shooting capability

---
+++ Robot Quidditch/Assets/Scripts/BallShooter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooter : MonoBehaviour
+{
+	public GameObject ball;
+
+	private float speed;
+	private Rigidbody rb;
+
+	
+	// Use this for initialization
+	void Start ()
+	{
+		speed = ball.GetComponent<Ball>().speed;
+		rb = ball.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		// Ctrl was pressed, launch a projectile
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Transform t = transform;
+			// Instantiate the projectile at the position of the barrel and translate along the barrel by its length
+			var length = t.localScale.y;
+			var direction = t.TransformDirection(Vector3.up);
+			var clone = Instantiate<Rigidbody>(rb, 
+				t.position + length * direction,
+				Quaternion.identity);
+			clone.velocity = speed * direction;
+		}
+	}
+}
