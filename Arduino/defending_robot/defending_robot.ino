c15f76801c5017f32ef53f3a38f55dba8e120981
Jens Hannemann <j.hannemann@ieee.org>
2019-04-21 12:02:44 -0400

    Add reversing on switch
    When the front switch is triggered, and the robot goes forward, it
    reverses direction and slightly backs off. Same for the back switch.
    When both switches are activated, or the switch opposite to the
    direction the robot is going is activated, the robot simply stops.
    If both switches are clear, the robot resumes line following.

--- Arduino/defending_robot/defending_robot.ino
+++ Arduino/defending_robot/defending_robot.ino
@@ -32,7 +32,7 @@ const int MAX_DISTANCE = 150; //cm
 const int QTI_CHARGE_TIME = 800; // microseconds
 const int QTI_DISCHARGE_TIME = 800; // microseconds
 const int STARTUP_DELAY = 2000; // milliseconds
-const unsigned long DEBOUNCE_DELAY = 100; // milliseconds
+const unsigned long DEBOUNCE_DELAY = 10; // milliseconds
 const unsigned DISTANCE_ARRAY_SIZE = 16;
 
 const int TRIG_PIN = 10;
@@ -43,10 +43,35 @@ const int SERVO_R_PIN = 13;
 const int SWITCH_F_PIN = 8;
 const int SWITCH_B_PIN = 9;
 
+class Switch {
+public:
+  Switch(int pin) : m_pin(pin), m_switchState(HIGH),
+                    m_lastSwitchState(HIGH), m_lastDebounceTime(0) {}
+  bool state() {
+    bool switchReading = digitalRead(m_pin);
+    if(switchReading != m_lastSwitchState) {
+      // reset the debouncing timer
+      m_lastDebounceTime = millis();
+    }
+    if((millis() - m_lastDebounceTime) > DEBOUNCE_DELAY) {
+      if(switchReading != m_switchState) {
+        m_switchState = switchReading;
+      }
+    }
+    m_lastSwitchState = switchReading;
+    return m_switchState;
+  }
+private:
+  int m_pin;
+  bool m_switchState;
+  bool m_lastSwitchState;
+  unsigned long m_lastDebounceTime = 0;
+};
+
+Switch switchFront(SWITCH_F_PIN);
+Switch switchBack(SWITCH_B_PIN);
 
 bool reverse = false;
-bool switchState;
-bool lastSwitchState = true;
 unsigned long lastDebounceTime = 0;
 // use a average for distance to act as a low-pass filter
 int distances[DISTANCE_ARRAY_SIZE];
@@ -57,6 +82,7 @@ Servo servoR;
 
 void calibrationSequence() {
   stop();
+  delay(3000);
   forward();
   delay(3000);
   stop();
@@ -138,7 +164,8 @@ void setup()
   servoR.attach(SERVO_R_PIN);
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT); 
-  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  pinMode(SWITCH_F_PIN, INPUT_PULLUP);
+  pinMode(SWITCH_B_PIN, INPUT_PULLUP);
   stop();
   delay(STARTUP_DELAY);
   //calibrationSequence();
@@ -158,31 +185,38 @@ void loop()
 
 #ifndef NDEBUG
   Serial.print("Pins: ");
-  //Serial.print((bool)(pins & B1000));
   Serial.print((bool)(pins & B0100));
   Serial.print((bool)(pins & B0010));
   Serial.print((bool)(pins & B0001));
 #endif
 
-//  bool switchReading = digitalRead(SWITCH_PIN);
-//  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-//      // whatever the reading is at, it's been there for longer than the debounce
-//      // delay, so take it as the actual current state:
-//  
-//      // if the button state has changed:
-//      if (switchReading != switchState) {
-//        switchState = switchReading;
-//  
-//        if (switchState == LOW) {
-//          reverse = !reverse;
-//          stop();
-//        }
-//      }
-//  }
+  bool switchFrontReading = switchFront.state();
+  bool switchBackReading = switchBack.state();
+  if(switchFrontReading == LOW && switchBackReading == LOW) {
+    stop();
+  }
+  else if(switchFrontReading == LOW) {
+    stop();
+    if(!reverse) {
+      reverse = true;
+      forward();
+      delay(DEBOUNCE_DELAY);
+    }
+  }
+  else if(switchBackReading == LOW) {
+    stop();
+    if(reverse){
+      reverse = false;
+      forward();
+      delay(DEBOUNCE_DELAY);
+    }
+  }
 
 #ifndef NDEBUG
-  Serial.print(", Switch: "); 
-//  Serial.print(switchReading);
+  Serial.print(", Switch F: "); 
+  Serial.print(switchFrontReading);
+  Serial.print(", Switch B: "); 
+  Serial.print(switchBackReading);
   Serial.print(", Reverse: ");
   Serial.println(reverse);
 #endif
@@ -199,67 +233,26 @@ void loop()
 //  else
 //    forward();
 
-  switch(pins) {
-    case B100:
-      turnLeft(3);
-      break;
-    case B110:
-      turnLeft(2);
-      break;
-    case B010:
-    case B111:
-      forward();
-      break;
-    case B001:
-      turnRight(3);
-      break;
-    case B011:
-      turnRight(2);
-      break;
-    default:
-      forward();
+  if(switchFrontReading == HIGH && switchBackReading == HIGH) {
+    switch(pins) {
+      case B100:
+        turnLeft(3);
+        break;
+      case B110:
+        turnLeft(2);
+        break;
+      case B010:
+      case B111:
+        forward();
+        break;
+      case B001:
+        turnRight(3);
+        break;
+      case B011:
+        turnRight(2);
+        break;
+      default:
+        forward();
+    }
   }
-
-//turnRight(3);
-//delay(3000);
-//turnLeft(3);
-//delay(3000);
-
-//  switch(pins) {
-//    case B0001:
-//      if(reverse)
-//        turnLeft(2);
-//      else
-//        turnRight(2);
-//      break;
-//    case B0011:
-//    case B0100:
-//    case B0111:
-//      if(reverse)
-//        turnLeft(1);
-//      else
-//        turnRight(1);
-//      break;
-//    case B0110:
-//      forward();
-//      break;
-//    case B0010:
-//    case B1100:
-//    case B1110:
-//      if(reverse)
-//        turnRight(1);
-//      else
-//        turnLeft(1);
-//      break;
-//    case B1000:
-//      if(reverse)
-//        turnRight(2);
-//      else
-//        turnLeft(2);
-//      break;
-//    default:
-//      stop();
-//}
-  
-//  delay(50);
 }
